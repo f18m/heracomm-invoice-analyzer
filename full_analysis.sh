9b45ae415442320c727c558f92effd17d1b7e110
f18m <francesco.montorsi@gmail.com>
2025-11-19 22:59:11 +0100

    translate in english; add real readme

--- full_analysis.sh
+++ full_analysis.sh
@@ -4,13 +4,13 @@ FOLDER="${1:-./francesco-electricity}"
 
 echo "Selected folder: ${FOLDER}"
 
-./step1_analizza_bollette_hera.py ${FOLDER} \
+./step1_invoice_analyzer.py ${FOLDER} \
     --output-csv ${FOLDER}/bollette_hera_riepilogo.csv
 
-./step2_interpolazione.py \
+./step2_interpolate.py \
     --input ${FOLDER}/bollette_hera_riepilogo.csv \
     --output-csv ${FOLDER}/bollette_hera_riepilogo_processato.csv
 
-./step3_crea_pagina_html.py \
+./step3_create_html_page.py \
     --input ${FOLDER}/bollette_hera_riepilogo_processato.csv \
     --output-html ${FOLDER}/bollette_hera_riepilogo_interattivo.html
